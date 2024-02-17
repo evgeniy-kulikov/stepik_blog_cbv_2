1b8fe0d886039c2139e4e9a8155085362f4494df
evgeniy-kulikov <redstepway@gmail.com>
2024-02-17 15:48:38 +0300

    доступ к параметрам on_each_side, on_ends из get_elided_page_range

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -9,13 +9,37 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    paginate_by = 2
+    paginate_by = 1
     queryset = Post.custom.all()  # Переопределение вызова модели
 
     # get_context_data - может использоваться для передачи содержимого или параметров вне модели в шаблон
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+
+        # получить доступ к параметрам on_each_side,  on_ends
+        # https://django.fun/docs/django/5.0/ref/paginator/#django.core.paginator.Paginator
+        """
+        Paginator.get_elided_page_range(number, *, on_each_side=3, on_ends=2)[исходный код]¶
+        Возвращает основанный на 1 список номеров страниц, аналогичный Paginator.page_range, 
+        но может добавлять многоточие с одной или обеих сторон текущего номера страницы, 
+        если Paginator.num_pages имеет большой размер.
+
+        Количество страниц, включаемых с каждой стороны от текущего номера страницы, 
+        определяется аргументом on_each_side, который по умолчанию равен 3.
+
+        Количество страниц, включаемых в начало и конец диапазона страниц, 
+        определяется аргументом on_ends, который по умолчанию равен 2.
+
+        Например, при значениях по умолчанию для on_each_side и on_ends, 
+        если текущая страница - 10, а всего страниц 50, 
+        диапазон страниц будет [1, 2, '…', 7, 8, 9, 10, 11, 12, 13, '…', 49, 50]. 
+        Это приведет к появлению страниц 7, 8 и 9 слева и 11, 12 и 13 справа от текущей страницы, 
+        а также страниц 1 и 2 в начале и 49 и 50 в конце.
+        """
+        page = context['page_obj']
+        context['paginator_range'] = page.paginator.get_elided_page_range(page.number, on_each_side=2, on_ends=2)
+
         return context
 
 
